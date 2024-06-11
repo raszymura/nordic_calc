59d179796aa010adc09cdf054b4b68db9ac6a94c
Rafał Szymura <raszymura@gmail.com>
2024-06-11 16:39:45 +0200

    demo: write_notify

---
+++ demo/write_notify/src/my_lbs.c
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/** @file
+ *  @brief LED Button Service (LBS) sample
+ */
+
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+
+#include "my_lbs.h"
+
+LOG_MODULE_DECLARE(Lesson4_Exercise2);
+
+static bool notify_mysensor_enabled;
+static struct my_lbs_cb lbs_cb;
+
+/* STEP 13 - Define the configuration change callback function for the MYSENSOR characteristic */
+static void mylbsbc_ccc_mysensor_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value)
+{
+	notify_mysensor_enabled = (value == BT_GATT_CCC_NOTIFY);
+}
+
+static ssize_t write_led(struct bt_conn *conn, const struct bt_gatt_attr *attr, const void *buf,
+			 uint16_t len, uint16_t offset, uint8_t flags)
+{
+	LOG_DBG("Attribute write, handle: %u, conn: %p", attr->handle, (void *)conn);
+
+	if (len != 1U) {
+		LOG_DBG("Write led: Incorrect data length");
+		return BT_GATT_ERR(BT_ATT_ERR_INVALID_ATTRIBUTE_LEN);
+	}
+
+	if (offset != 0) {
+		LOG_DBG("Write led: Incorrect data offset");
+		return BT_GATT_ERR(BT_ATT_ERR_INVALID_OFFSET);
+	}
+
+	if (lbs_cb.led_cb) {
+		// Read the received value
+		uint8_t val = *((uint8_t *)buf);
+
+		if (val == 0x00 || val == 0x01) {
+			// Call the application callback function to update the LED state
+			lbs_cb.led_cb(val ? true : false);
+		} else {
+			LOG_DBG("Write led: Incorrect value");
+			return BT_GATT_ERR(BT_ATT_ERR_VALUE_NOT_ALLOWED);
+		}
+	}
+
+	return len;
+}
+
+/* LED Button Service Declaration */
+BT_GATT_SERVICE_DEFINE(
+	my_lbs_svc, BT_GATT_PRIMARY_SERVICE(BT_UUID_LBS),
+
+	BT_GATT_CHARACTERISTIC(BT_UUID_LBS_LED, BT_GATT_CHRC_WRITE, BT_GATT_PERM_WRITE, NULL,
+			       write_led, NULL),
+	/* STEP 12 - Create and add the MYSENSOR characteristic and its CCCD  */
+	BT_GATT_CHARACTERISTIC(BT_UUID_LBS_MYSENSOR, BT_GATT_CHRC_NOTIFY, BT_GATT_PERM_NONE, NULL, NULL, NULL),
+	BT_GATT_CCC(mylbsbc_ccc_mysensor_cfg_changed, BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+);
+
+/* A function to register application callbacks for the LED and Button characteristics  */
+int my_lbs_init(struct my_lbs_cb *callbacks)
+{
+	if (callbacks) {
+		lbs_cb.led_cb = callbacks->led_cb;
+	}
+
+	return 0;
+}
+
+/* STEP 14 - Define the function to send notifications for the MYSENSOR characteristic */
+int my_lbs_send_sensor_notify(uint32_t sensor_value)
+{
+	if (!notify_mysensor_enabled) {
+		return -EACCES;
+	}
+
+	return bt_gatt_notify(NULL, &my_lbs_svc.attrs[4], &sensor_value, sizeof(sensor_value));
+}
